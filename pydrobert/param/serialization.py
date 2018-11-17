5f99d0f0f8be7d4691c56ae7512964553194b4cd
Sean Robertson <sdrobert@cs.toronto.edu>
2018-11-16 19:25:34 -0500

    Added boilerplate

---
+++ pydrobert/param/serialization.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import param
+
+__author__ = "Sean Robertson"
+__email__ = "sdrobert@cs.toronto.edu"
+__license__ = "Apache 2.0"
+__copyright__ = "Copyright 2018 Sean Robertson"
