29b44c8b4610514889509737a440866ce1df9a65
Sean Robertson <sdrobert@cs.toronto.edu>
2019-07-10 18:53:38 -0400

    Py 2.7 fix

--- pydrobert/param/serialization.py
+++ pydrobert/param/serialization.py
@@ -977,8 +977,10 @@ def _serialize_to_ruamel_yaml(ruamel_yaml, fp, obj, help_dict=None):
         return _serialize_to_ruamel_yaml_help_dict(
             ruamel_yaml, fp, obj, help_dict)
     elif isinstance(obj, OrderedDict):
-        # round-trip dump will use !!omap if an ordered dict. Don't want that
-        obj = ruamel_yaml.comments.CommentedMap(obj)
+        # round-trip dump will use !!omap if an ordered dict. Don't want that.
+        # py2.7 seems to think OrderedDict is unordered, so we update
+        new = ruamel_yaml.comments.CommentedMap()
+        new.update(obj)
     ruamel_yaml.YAML().dump(obj, stream=fp)
 
 
