284d8bfeb410381628b037ce286d494db145a3fc
Sean Robertson <sdrobert@cs.toronto.edu>
2022-08-19 16:31:33 -0400

    How does it look with sphinx-autodoc-typehints?

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,7 +40,7 @@ extensions = [
     "sphinx.ext.viewcode",
     "sphinx.ext.intersphinx",
     "sphinx.ext.napoleon",
-    # "sphinx_autodoc_typehints",
+    "sphinx_autodoc_typehints",
 ]
 
 napoleon_numpy_docstring = True
